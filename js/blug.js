b2b779695e9c80d54a3ef0bf2a66251758827f04
hudahussein <h.hussein.83@gmail.com>
2019-02-15 20:15:21 +0200

    first commit

---
+++ js/blug.js
@@ -0,0 +1,23 @@
+/*global $, document,*/
+
+$(document).scroll(function () {
+    "use strict";
+    
+    $('.navbar').toggleClass('scrolled', $(this).scrollTop() > $('.navbar').height());
+});
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 300,
+        modifier: 1,
+        slideShadows: true,
+    },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
